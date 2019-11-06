101c57629db096b397e8995d417d4b0dd27f7c87
etienne <etienne.monier@enseeiht.fr>
2019-11-06 14:32:44 +0100

    Some sublime_text extra features.

--- __setup__/sublime_text.sh
+++ __setup__/sublime_text.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
-## Install Sublime-text-3 (cf ubuntu doc)
-wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
-sudo apt-get install apt-transport-https
- 
-# Stable version
-echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+if [ ! command -v sublime_text >/dev/null 2>&1 ]
 
-# Install
-sudo apt-get update
-sudo apt-get install sublime-text
+  ## Install Sublime-text-3 (cf ubuntu doc)
+  wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+  sudo apt-get install apt-transport-https
+ 
+  # Stable version
+  echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+  
+  # Install
+  sudo apt-get update
+  sudo apt-get install sublime-text
+
+fi
 
 ## Re-launch .zshrc for PATH location and launch sublime_text for config file init.
 source $HOME/.zshrc
@@ -21,12 +25,11 @@ CUR_DIR=`pwd`
 DIR="$HOME/.config/sublime-text-3/Packages"
 
 ## Install plugins
-
 cd $DIR
+
 
 # Install Package Control
 wget -O "$HOME/.config/sublime-text-3/Installed Packages/Package Control.sublime-package" https://packagecontrol.io/Package%20Control.sublime-package
-# git clone https://github.com/wbond/package_control.git "Package Control"
 
 # AlignTab
 git clone https://github.com/randy3k/AlignTab.git
@@ -49,3 +52,19 @@ git clone https://github.com/gsemet/pythonpep8autoformat.git
 # WordCount
 git clone https://github.com/titoBouzout/WordCount.git WordCount 
 
+# LaTeXtools
+git clone https://github.com/SublimeText/LaTeXTools.git
+
+# LaTeX-cwl
+git clone https://github.com/LaTeXing/LaTeX-cwl.git
+
+# RST completion
+git clone https://github.com/mgaitan/sublime-rst-completion.git
+
+# Trailing spaces
+git clone https://github.com/SublimeText/TrailingSpaces.git
+
+# French Dico
+git clone https://github.com/superbob/SublimeTextLanguageFrench.git FrenchDico
+
+cd $CUR_DIR
