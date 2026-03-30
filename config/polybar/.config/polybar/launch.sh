f197c7f93d3fb26576211b33ca348356884f2f4c
Etienne Monier <etienne.monier@protonmail.com>
2026-03-30 14:11:14 +0200

    fix: Add polybar to any monitor

--- config/polybar/.config/polybar/launch.sh
+++ config/polybar/.config/polybar/launch.sh
@@ -6,11 +6,19 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch correct bars
-HOSTNAME=$(hostname)
+# # Launch correct bars
+# HOSTNAME=$(hostname)
 
-case $HOSTNAME in
-*) polybar ;;
-esac
+# case $HOSTNAME in
+# *) polybar ;;
+# esac
 
-echo "Bars launched..."
+# echo "Bars launched..."
+
+
+# Launch one bar per monitor
+for monitor in $(polybar --list-monitors | cut -d":" -f1); do
+  MONITOR=$monitor polybar --reload default &
+done
+
+echo "Polybar launched on all monitors"
